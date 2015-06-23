e2f13decd688617ed867502bdbca6685dfaee9e7
phillyfan1138 <danstahl1138@gmail.com>
2015-06-23 06:03:26 -0400

    moved database to own class

--- server.js
+++ server.js
@@ -5,84 +5,8 @@ var server  = require('http').createServer(app); //required fro socket.io
 var io= require('socket.io').listen(server);
 
 
-
-
-var url = 'mongodb://localhost:27017/myTestDB';
-var collections = ["visitors"];
-//var db = require("mongodb").connect(databaseURI, collections);
-
-/*Database utility: mongodb which writes the timestamp and page that was viewed */
-var MongoClient = require('mongodb').MongoClient;
-var id="";//will this work trully asyncronously???  or will global variable be updated and override?
-function insertData(data, collection){
-    MongoClient.connect(url, function(err, db) {
-        if(err){
-            console.log(err);
-        }
-        else{
-            db.collection(collection).insert(data, function(err, result){
-                if(err){
-                    console.log(err);
-                }
-                else{
-                    console.log("Success");
-                    id=result.ops[0]['_id']; //if this works ill be shocked!  it doesnt work, not asynced...
-                    console.log("Insert ID: "+id);
-                    db.close();
-                   // callback(result);
-                }
-            });
-        }
-    });
-}
-function updateData(id, field, data, collection){
-    console.log("Update ID: "+id+" "+field);
-    MongoClient.connect(url, function(err, db) {
-        if(err){
-            console.log(err);
-        }
-        else{
-            db.collection(collection).updateOne({"_id":id}, {$set:{field:data}}, function(err, result){ //update "dateLeft" so we can get average time spent, etc
-                if(err){
-                    console.log(err);
-                }
-                else{
-                    console.log("Success Update");
-                    console.log(result.result.nModified);
-                    //id=result.ops[0]._id; //if this works ill be shocked!
-                    db.close();
-                    //callback(result);
-                }
-            });
-        }
-    });
-}
-
-function retrieveData(groupField, field, collection, callback){ //eventaully I want "GroupType" to not just be sum...
-    field="$"+field;
-    MongoClient.connect(url, function(err, db) {
-        if(err){
-            console.log(err);
-        }
-        else{
-            db.collection(collection).aggregate([
-                {$group:{"_id":field, "value":{$sum:groupField}}}
-            ]).toArray(function(err, result){ //update "dateLeft" so we can get average time spent, etc
-                if(err){
-                    console.log(err);
-                }
-                else{
-                    console.log("Success");
-                    //console.log(result);
-                    //id=result.ops[0]._id; //if this works ill be shocked!
-                    db.close();
-                    callback(result);
-                }
-            });
-        }
-    });
-}
-/*End database utility */
+var mongoUtils=require('mongoUtils');
+var myDatabase=new mongoUtils({url:"mongodb://localhost:27017/myTestDB", collections:"visitors"}); //default options work here...
 
 app.set('view engine', 'ejs');
 
@@ -119,12 +43,7 @@ myWebsiteLayout.forEach(function(value){
 function renderPage(file, link, name, menuName){
      app.get('/'+file, function(req, res){
         res.render(file, {linkage:link, name:name, menuNames:menuName});
-        insertData({page:file, dateEntered:new Date(), dateLeft:null}, "visitors"
-        //function(){
-         //   retrieveData(groupField, field, collection, callback)
-        //}
-        
-        ); //insert data into database
+        myDatabase.insertData({page:file, dateEntered:new Date(), dateLeft:null});
     });
 }
 app.get('/SiteStatistics', function(req, res){
@@ -132,7 +51,7 @@ app.get('/SiteStatistics', function(req, res){
 });
 io.on('connection', function(socket){
   console.log('a user connected');
-  retrieveData(1, "page", "visitors", function(data){return io.emit('updateData', data);});
+  myDatabase.retrieveData(1, "page", function(data){return io.emit('updateData', data);});
   
   socket.on('disconnect', function(){
     //updateData(id, "dateLeft", new Date(), "visitors"); //this doesn't work, global id
